4aaf32abc33c642ec766f239a9babcfed198c97a
ArpitaGavanang <162104524+ArpitaGavanang@users.noreply.github.com>
2024-09-05 18:06:22 +0530

    Delete src directory

--- src/main/java/com/RouteGaurd/app/repositories/DriverRepository.java
+++
@@ -1,9 +0,0 @@
-package com.RouteGaurd.app.repositories;
-
-import com.RouteGaurd.app.model.DriverMaster;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DriverRepository extends JpaRepository<DriverMaster, Long> {
-}
--- src/main/java/com/RouteGaurd/app/repositories/MaintenanceHistoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.RouteGaurd.app.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.RouteGaurd.app.model.MaintenanceHistory;
-
-public interface MaintenanceHistoryRepository extends JpaRepository<MaintenanceHistory, Long> {
-}
-
